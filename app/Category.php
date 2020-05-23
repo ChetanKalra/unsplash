092d29b9a887ec7ff75831e3f1686e02be92ac6a
Chetan Kalra <chetankalra11@gmail.com>
2020-05-23 13:33:09 +0530

    Relationships in Laravel

--- app/Category.php
+++ app/Category.php
@@ -7,4 +7,53 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     protected $guarded = ['id'];
+
+
+    public function photos()
+    {
+        return $this->hasMany(Photo::class);
+    }
 }
+
+
+// college
+// classes
+// students
+// teachers
+
+// Model - College, Migration create_colleges_table
+
+// One to Many between College & Class
+
+// College
+// public function classes()
+// {
+//     return $this->hasMany(Class::class);
+// }
+
+// // Class
+// public function college()
+// {
+//     return $this->belongsTo(College::class);
+// }
+
+// public function students()
+// {
+//     return $this->hasMany(Student::class);
+// }
+
+// // Student
+// public function class()
+// {
+//     return $this->belongsTo(Class::class);
+// }
+
+
+// One to One
+// hasOne,   Inverse - belongsTo
+
+// One to Many
+// hasMany,    Inverse - belongsTo
+
+// Many to Many
+// belongsToMany
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\User;
+use App\Profile;
+use App\Category;
+use App\Photo;
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -17,3 +22,28 @@ Route::get('/edit/{id}', 'CategoryController@edit')->name('category.edit');
 Route::post('/categories/update/{id}', 'CategoryController@update')->name('category.update');
 
 Route::get('/categories/delete/{id}', 'CategoryController@delete')->name('category.delete');
+
+Route::get('/test', function(){
+
+    // $user = User::find(1);
+
+    // return $user->profile;
+
+    // $profile = Profile::where('user_id', $user->id)->first();
+
+    // return $profile;
+
+
+    // $profile = Profile::find(1);
+
+    // return $profile->user;
+
+
+    // $category = Category::find(4);
+    // return $category->photos;
+
+    $photo = Photo::find(2);
+
+    return $photo->category;
+
+});
